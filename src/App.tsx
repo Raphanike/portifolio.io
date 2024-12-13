807ac6bde1ea85d3bbf35bd39d3398f523f131ff
Your Name <you@example.com>
2024-12-12 22:04:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <div>
+      <EstiloGlobal />
+      <Teste />
+    </div>
+  )
+}
+
+export default App
